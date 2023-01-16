d191463da93543a7681c05686476246653f11ee3
saif <adrian.migraso@gmail.com>
2023-01-17 06:00:24 +0800

    add patch sqlite-storage

---
+++ patch/patchscript.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -vf ../node_modules/react-native-sqlite-storage/platforms/android-native/libs/sqlite-connector.jar
+
+patch -N ../node_modules/react-native-sqlite-storage/platforms/android-native/gradle.properties sqlite-storage.patch
+
+cp -vf srclibs/Android-sqlite-connector/src/io/liteglue/* ../node_modules/react-native-sqlite-storage/platforms/android-native/src/main/java/io/liteglue/
